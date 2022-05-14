5b8511a6b3fd6f341ccd6f37552655e9417f5768
Thomas Quinton <91420588+SleepFast@users.noreply.github.com>
2022-05-14 17:54:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import { BrowserRouter } from 'react-router-dom'
-import App from './App';
+import App from './src/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
