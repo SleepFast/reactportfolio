29075074155135abf0177933b1015bdf239e133e
Thomas Quinton <cartmam1@hotmail.fr>
2022-06-24 19:33:42 +0200

    card Component almost end

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,5 @@
-import "./Card.scss" 
+import "./Card.scss";
+import ArrowSlide from '../Icons/arrowSlide';
  
 function Card() {
  
@@ -8,11 +9,12 @@ function Card() {
         <img class="Card-img" src={require('../../images/hangit.png')} alt="Teaser projet hangit" />
       </div>
       <div className="Card-content">
-        <h3 className="Card-title">Site Hangit hébargé sur github</h3>
+        <h3 className="Card-title">Site Hangit hébergé sur github</h3>
         <p className="Card-description">Projet site en html, css, js, et php avec une bdd MySQL. C'est un jeux en ligne jouable à plusieurs, qui reprend les règles du jeu du 'pendu'.</p>
-        <div className="Card-footer">
-          <a className="Card-link" href="https://sleepfast.github.io/Hangit.io/">Allez voir ça</a>
-        </div>
+      </div>
+      <div className="Card-footer">
+        <a className="Card-link" href="https://sleepfast.github.io/Hangit.io/">Voir le projet</a>
+        <ArrowSlide/>
       </div>
     </div> 
   ); 
